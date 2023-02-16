4a4264d26bcf07ab3cd2dd155c5e3427146124bd
Adrián coll <nege2002@gmail.com>
2023-02-16 19:44:58 +0100

    feat: debugger bar

---
+++ src/components/ui/Debugger.jsx
@@ -0,0 +1,20 @@
+import { useDebugger } from '../../hooks/useDebugger'
+
+export const Debugger = () => {
+  const { products } = useDebugger()
+
+  return (
+    <div className='fixed bottom-5 left-5 h-20 right-5'>
+      <span className='absolute z-10 -top-4 -left-3 bg-gray-900 text-gray-300/50 px-2 py-1 text-xs rounded-xl shadow-lg'>
+        Debugger
+      </span>
+      <div className='fixed bottom-5 left-5 h-20 right-5 overflow-auto bg-gray-900/50 shadow-lg backdrop-blur-lg rounded-xl px-4 py-2'>
+        <pre>
+          {JSON.stringify({
+            products
+          })}
+        </pre>
+      </div>
+    </div>
+  )
+}
