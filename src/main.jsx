21a77b4a2607b2166c9226b0b5abb23b23edfac9
Adrián coll <nege2002@gmail.com>
2023-02-16 19:41:46 +0100

    feat: wrap the appon the providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import './index.css'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { RouterProvider } from 'react-router-dom'
+import React from 'react'
+import { ProductProvider } from './context/products'
+import { CartProvider } from './context/cart'
+
+import { router } from './navigation/router'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ProductProvider>
+      <CartProvider>
+        <RouterProvider router={router} />
+      </CartProvider>
+    </ProductProvider>
   </React.StrictMode>
 )
