238554e7b455339b0646a7de92ca4521ad61ba85
Adrián coll <nege2002@gmail.com>
2023-02-17 09:50:13 +0100

    feat: improve search view

--- src/components/ui/products/ProductList.jsx
+++ src/components/ui/products/ProductList.jsx
@@ -3,6 +3,8 @@ import { ProductCard } from './ProductCard'
 import { useProducts } from '../../../hooks/useProducts'
 import { ProductSearchBar } from './ProductSearchBar'
 import { useDebounce } from '../../../hooks'
+import { Transition } from '@headlessui/react'
+import { FolderMinusIcon, FunnelIcon } from '@heroicons/react/24/outline'
 
 export const ProductList = () => {
   const [query, setQuery] = useState('')
@@ -18,36 +20,78 @@ export const ProductList = () => {
             .toLowerCase()
             .replace(/\s+/g, '')
             .includes(debouncedQuery.toLowerCase().replace(/\s+/g, ''))
-      )
+        )
 
   return (
     <div className='mx-auto max-w-2xl py-6 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 flex flex-col gap-8'>
-      <ProductSearchBar query={query} setQuery={setQuery} />
+      <ProductSearchBar
+        disabled={products.length === 0}
+        query={query}
+        setQuery={setQuery}
+      />
 
       <div>
         <h2 className='sr-only'>Dispositivos móviles</h2>
 
-        <div className='grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
-          {filteredProducts.map((product) => (
-            <ProductCard product={product} key={product._id} />
-          ))}
+        <Transition
+          show={filteredProducts.length > 0}
+          enter='transition-opacity duration-75'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='transition-opacity duration-150'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <div className='grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
+            {filteredProducts.map((product) => (
+              <ProductCard product={product} key={product._id} />
+            ))}
+          </div>
+        </Transition>
 
-          {filteredProducts.length === 0 && query !== '' ? (
-            <div className='flex items-center justify-center flex-col text-center gap-4'>
-              <p>
-                No se han encontrado productos con el nombre/marca{' '}
-                <strong>"{query}"</strong>
+        <Transition
+          show={filteredProducts.length === 0 && query !== ''}
+          enter='transition-opacity duration-75'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='transition-opacity duration-150'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <div className='grid place-items-center w-full py-52 '>
+            <FunnelIcon className='h-16 w-16' />
+            <div className='my-7 text-center'>
+              <p className='font-bold text-xl'>
+                No se han encontrado productos
               </p>
-              <button className='bg-indigo-500 text-white font-bold w-fit px-4 py-2 rounded-lg'>
-                Limpiar búsqueda
-              </button>
+              <p className='text-gray-400'>
+                No hay ningún dispositvo que encaje con tu búsqueda "
+                <strong>{query}</strong>"
+              </p>
             </div>
-          ) : null}
+            <button className='bg-indigo-600 text-white font-bold w-fit px-6 py-2 rounded-lg'>
+              Limpiar búsqueda
+            </button>
+          </div>
+        </Transition>
 
-          {filteredProducts.length === 0 ? (
-            <p>No se han encontrado productos</p>
-          ) : null}
-        </div>
+        <Transition
+          show={filteredProducts.length === 0 && query === ''}
+          enter='transition-opacity duration-75'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='transition-opacity duration-150'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <div className='grid place-items-center w-full py-52 text-xl'>
+            <FolderMinusIcon className='h-16 w-16' />
+            <div className='my-7 text-center'>
+              <p className='font-bold'>No se han encontrado productos</p>
+              <p className='text-gray-400'>Puede que haya un error!</p>
+            </div>
+          </div>
+        </Transition>
       </div>
     </div>
   )
--- src/components/ui/products/ProductSearchBar.jsx
+++ src/components/ui/products/ProductSearchBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const ProductSearchBar = ({ query, setQuery }) => {
+export const ProductSearchBar = ({ query, setQuery, disabled = false }) => {
   return (
     <div className='flex justify-end'>
       <label
@@ -31,8 +31,9 @@ export const ProductSearchBar = ({ query, setQuery }) => {
           onChange={(ev) => setQuery(ev.target.value)}
           type='search'
           id='search'
+          disabled={disabled}
           value={query}
-          className='block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          className='transition disabled:cursor-not-allowed disabled:opacity-50 block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
           placeholder='Buscar...'
           required
         />
