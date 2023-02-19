12210b26bd0294859d3373305619f96aa13cda72
Adrián coll <nege2002@gmail.com>
2023-02-19 04:16:16 +0100

    fix: skeleton

--- src/components/ui/products/ProductCartSkeleton.jsx
+++ src/components/ui/products/ProductCartSkeleton.jsx
@@ -22,9 +22,9 @@ export const ProductCartSkeleton = () => {
           </div>
           <div className='w-full mt-4'>
             <div className='flex w-full justify-between items-center'>
-              <div className='h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-40 mb-4'></div>
-              <div className='h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-20 mb-2.5'></div>
+              <div className='h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-40 mb-4' />
             </div>
+            <div className='h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-12' />
           </div>
           <span className='sr-only'>Loading...</span>
         </div>
